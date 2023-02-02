c06e23ec138d64c93e7d45449b21c1f5c342e863
UjjwalJha131 <ujjwal.ukj@gmail.com>
2023-02-02 12:18:45 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,61 @@
+let searchBar = document.getElementById("search-bar");
+
+
+const weather = {
+  apiKey: keys.api,
+
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+
+    document.getElementById("city").innerText = "Weather in " + name;
+
+    document.getElementById("icon").src =
+      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+    document.getElementById("description").innerText = description;
+
+    document.getElementById("temp").innerText = temp + "°C";
+
+    document.getElementById("humidity").innerText =
+      "Humidity is " + humidity + " %";
+
+    document.getElementById("wind").innerText =
+      "Wind speed is " + speed + " km/h";
+
+      document.querySelector(".weather").classList.remove("loading");
+
+      /*document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"*/
+  },
+
+  
+  search: () => {
+    weather.fetchWeather(document.getElementById("search-bar").value);
+  }
+
+};
+
+document.querySelector(".search button").addEventListener("click", () => {
+  weather.search();
+});
+
+searchBar.addEventListener("keyup" , (event)=> {
+    if(event.key == "Enter") {
+      weather.search();
+    }
+});
+
+weather.fetchWeather("Goa")
